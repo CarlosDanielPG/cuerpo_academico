cd4fe3acbf659290ce698f14580ff07e64c5c5cf
Christian <christians.vargas@alumnos.udg.mx>
2018-05-14 13:22:25 -0500

    Puto shamir

--- Cuerpo_Academico/Carreras.Designer.cs
+++ Cuerpo_Academico/Carreras.Designer.cs
@@ -28,7 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblNombreCarrera = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cmbDivisiones = new System.Windows.Forms.ComboBox();
+            this.lblNombreDivision = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.gbBusqueda = new System.Windows.Forms.GroupBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.dgvCarreras = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.division = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nivel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmbNivel = new System.Windows.Forms.ComboBox();
+            this.lblNivel = new System.Windows.Forms.Label();
+            this.gbBusqueda.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarreras)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblNombreCarrera
+            // 
+            this.lblNombreCarrera.AutoSize = true;
+            this.lblNombreCarrera.Location = new System.Drawing.Point(19, 23);
+            this.lblNombreCarrera.Name = "lblNombreCarrera";
+            this.lblNombreCarrera.Size = new System.Drawing.Size(110, 13);
+            this.lblNombreCarrera.TabIndex = 0;
+            this.lblNombreCarrera.Text = "Nombre de la Carrera:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(22, 39);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(212, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // cmbDivisiones
+            // 
+            this.cmbDivisiones.FormattingEnabled = true;
+            this.cmbDivisiones.Location = new System.Drawing.Point(22, 88);
+            this.cmbDivisiones.Name = "cmbDivisiones";
+            this.cmbDivisiones.Size = new System.Drawing.Size(121, 21);
+            this.cmbDivisiones.TabIndex = 2;
+            // 
+            // lblNombreDivision
+            // 
+            this.lblNombreDivision.AutoSize = true;
+            this.lblNombreDivision.Location = new System.Drawing.Point(19, 72);
+            this.lblNombreDivision.Name = "lblNombreDivision";
+            this.lblNombreDivision.Size = new System.Drawing.Size(113, 13);
+            this.lblNombreDivision.TabIndex = 3;
+            this.lblNombreDivision.Text = "Nombre de la División:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(22, 126);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(85, 23);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(122, 126);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(85, 23);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // gbBusqueda
+            // 
+            this.gbBusqueda.Controls.Add(this.lblID);
+            this.gbBusqueda.Controls.Add(this.txtID);
+            this.gbBusqueda.Controls.Add(this.btnBuscar);
+            this.gbBusqueda.Controls.Add(this.btnEliminar);
+            this.gbBusqueda.Controls.Add(this.btnModificar);
+            this.gbBusqueda.Location = new System.Drawing.Point(322, 23);
+            this.gbBusqueda.Name = "gbBusqueda";
+            this.gbBusqueda.Size = new System.Drawing.Size(157, 173);
+            this.gbBusqueda.TabIndex = 51;
+            this.gbBusqueda.TabStop = false;
+            this.gbBusqueda.Text = "Busqueda";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(13, 18);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 13);
+            this.lblID.TabIndex = 28;
+            this.lblID.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(16, 40);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(129, 20);
+            this.txtID.TabIndex = 28;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(16, 71);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(129, 23);
+            this.btnBuscar.TabIndex = 16;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(16, 101);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(129, 23);
+            this.btnEliminar.TabIndex = 18;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(16, 133);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(129, 23);
+            this.btnModificar.TabIndex = 17;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // dgvCarreras
+            // 
+            this.dgvCarreras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarreras.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.nombre,
+            this.division,
+            this.nivel});
+            this.dgvCarreras.Location = new System.Drawing.Point(22, 216);
+            this.dgvCarreras.Name = "dgvCarreras";
+            this.dgvCarreras.Size = new System.Drawing.Size(573, 132);
+            this.dgvCarreras.TabIndex = 50;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Width = 130;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            this.nombre.Width = 200;
+            // 
+            // division
+            // 
+            this.division.HeaderText = "División";
+            this.division.Name = "division";
+            // 
+            // nivel
+            // 
+            this.nivel.HeaderText = "Nivel";
+            this.nivel.Name = "nivel";
+            // 
+            // cmbNivel
+            // 
+            this.cmbNivel.FormattingEnabled = true;
+            this.cmbNivel.Location = new System.Drawing.Point(166, 88);
+            this.cmbNivel.Name = "cmbNivel";
+            this.cmbNivel.Size = new System.Drawing.Size(121, 21);
+            this.cmbNivel.TabIndex = 52;
+            // 
+            // lblNivel
+            // 
+            this.lblNivel.AutoSize = true;
+            this.lblNivel.Location = new System.Drawing.Point(163, 72);
+            this.lblNivel.Name = "lblNivel";
+            this.lblNivel.Size = new System.Drawing.Size(34, 13);
+            this.lblNivel.TabIndex = 53;
+            this.lblNivel.Text = "Nivel:";
             // 
             // Carreras
             // 
@@ -36,13 +218,48 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(813, 399);
+            this.Controls.Add(this.lblNivel);
+            this.Controls.Add(this.cmbNivel);
+            this.Controls.Add(this.gbBusqueda);
+            this.Controls.Add(this.dgvCarreras);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblNombreDivision);
+            this.Controls.Add(this.cmbDivisiones);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombreCarrera);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Carreras";
             this.Text = "Carreras";
+            this.Load += new System.EventHandler(this.Carreras_Load);
+            this.gbBusqueda.ResumeLayout(false);
+            this.gbBusqueda.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarreras)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNombreCarrera;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox cmbDivisiones;
+        private System.Windows.Forms.Label lblNombreDivision;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.GroupBox gbBusqueda;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.DataGridView dgvCarreras;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn division;
+        private System.Windows.Forms.ComboBox cmbNivel;
+        private System.Windows.Forms.Label lblNivel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nivel;
     }
 }
