e43b450b03cd83dce3017e8b94b67478de422aba
CarlosDanielPG <carlospdcg@hotmail.com>
2018-05-14 12:57:13 -0500

    Added validation for combobox

--- Cuerpo_Academico/LineasInvestigacion.Designer.cs
+++ Cuerpo_Academico/LineasInvestigacion.Designer.cs
@@ -36,12 +36,17 @@
             this.btnBuscar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgvDivisiones = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.gbBusqueda.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDivisiones)).BeginInit();
             this.SuspendLayout();
             // 
             // txtDescripcion
             // 
-            this.txtDescripcion.Location = new System.Drawing.Point(26, 36);
+            this.txtDescripcion.Location = new System.Drawing.Point(26, 43);
             this.txtDescripcion.Name = "txtDescripcion";
             this.txtDescripcion.Size = new System.Drawing.Size(145, 20);
             this.txtDescripcion.TabIndex = 0;
@@ -62,7 +67,7 @@
             this.gbBusqueda.Controls.Add(this.btnBuscar);
             this.gbBusqueda.Controls.Add(this.btnEliminar);
             this.gbBusqueda.Controls.Add(this.btnModificar);
-            this.gbBusqueda.Location = new System.Drawing.Point(472, 20);
+            this.gbBusqueda.Location = new System.Drawing.Point(327, 33);
             this.gbBusqueda.Name = "gbBusqueda";
             this.gbBusqueda.Size = new System.Drawing.Size(157, 173);
             this.gbBusqueda.TabIndex = 49;
@@ -112,12 +117,46 @@
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = true;
             // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(188, 41);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(121, 23);
+            this.btnGuardar.TabIndex = 50;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // dgvDivisiones
+            // 
+            this.dgvDivisiones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDivisiones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.descripcion});
+            this.dgvDivisiones.Location = new System.Drawing.Point(490, 36);
+            this.dgvDivisiones.Name = "dgvDivisiones";
+            this.dgvDivisiones.Size = new System.Drawing.Size(303, 159);
+            this.dgvDivisiones.TabIndex = 51;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Width = 130;
+            // 
+            // descripcion
+            // 
+            this.descripcion.HeaderText = "Descripcion";
+            this.descripcion.Name = "descripcion";
+            this.descripcion.Width = 130;
+            // 
             // LineasInvestigacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(813, 399);
+            this.Controls.Add(this.dgvDivisiones);
+            this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.gbBusqueda);
             this.Controls.Add(this.lblDescripcion);
             this.Controls.Add(this.txtDescripcion);
@@ -126,6 +165,7 @@
             this.Text = "LineasInvestigacion";
             this.gbBusqueda.ResumeLayout(false);
             this.gbBusqueda.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDivisiones)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,5 +181,9 @@
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgvDivisiones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
     }
 }
