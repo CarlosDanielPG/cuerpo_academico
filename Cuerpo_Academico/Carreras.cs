fe4fae1af68a1ee5e5320171f2a580305a5a3336
Shamir Caro <39143191+ShamirCO@users.noreply.github.com>
2018-05-14 13:32:50 -0500

    FIX

--- Cuerpo_Academico/Carreras.cs
+++ Cuerpo_Academico/Carreras.cs
@@ -86,7 +86,11 @@ namespace Cuerpo_Academico
             List<ComboBox> comboBoxes = new List<ComboBox>();
             comboBoxes.Add(cmbDivisiones);
             comboBoxes.Add(cmbNivel);
-            if(!Validator.v)
+            if(!Validator.validateComboBoxes(comboBoxes))
+            {
+                MessageBox.Show("Seleccion un nivel y/o una division", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             consulta = "INSERT INTO carrera (nombre, id_nivel, id_division) VALUES('" + txtNombre.Text + "', " + (cmbNivel.SelectedItem as ComboBoxItem).Value.ToString() + ", " + (cmbDivisiones.SelectedItem as ComboBoxItem).Value.ToString() + ");";
             resultado = conexion.ejecutarComando(consulta);
             if (resultado.RecordsAffected > 0)
